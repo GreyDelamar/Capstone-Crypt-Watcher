c3c14318c6dda1a680e2eb76406e9058d2bd976e
Grey Delamar <grey.delamar@gmail.com>
2016-12-07 16:58:50 -0700

    fixed converter

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
-  // $('form').on('submit', function(e){
-  //   e.preventDefault();
-  // });
+  $('#converter').on('submit', function(e){
+    e.preventDefault();
+  });
   $.get("https://api.coinmarketcap.com/v1/ticker/")
     .then(function(Rates) {
-      console.log(Rates)
-google.charts.load('current', {'packages':['table']});
+      google.charts.load('current', {
+        'packages': ['table']
+      });
       google.charts.setOnLoadCallback(drawTable);
 
       function drawTable() {
@@ -15,18 +16,24 @@ google.charts.load('current', {'packages':['table']});
         data.addColumn('string', 'Cost in USD');
         data.addColumn('string', '% 1h');
         data.addColumn('string', '% 24h');
-        data.addRows(Rates.map(v => [v.id,v.symbol,v.price_usd,v.percent_change_1h,v.percent_change_24h]));
+        data.addRows(Rates.map(v => [v.id, v.symbol, v.price_usd, v.percent_change_1h, v.percent_change_24h]));
 
         var table = new google.visualization.Table(document.getElementById('table_div'));
 
-        table.draw(data, {showRowNumber: true, width: '100%', height: '100%', page:'enable', pageSize: 20});
+        table.draw(data, {
+          showRowNumber: true,
+          width: '100%',
+          height: '100%',
+          page: 'enable',
+          pageSize: 20
+        });
       }
       for (i in Rates) {
 
         var opt = $("<option>")
         $("#coinselect").append(opt);
         opt.text(Rates[i].name)
-        opt.attr("value", Rates[i].name)
+        opt.attr("value", Rates[i].price_btc)
 
         var btcexerate = (1 / Rates[0].price_usd)
         var pt1equation = (1 / btcexerate)
